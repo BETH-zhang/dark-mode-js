3175a714edaf38b4bb5e6f2585d78bf136e3d6b3
BETH-zhang <522718277@qq.com>
2021-10-19 19:26:16 +0800

    project init

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'src/index.js',  // 入口文件
+  output: {  // 输出 options
+    file: 'dist/bundle.js',  // 输出文件名
+    format: 'cjs'       // 输出格式
+  },
+  plugins: [ // 增加 plugins
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // 不对node_modules进行编译
+    })
+  ]
+}
